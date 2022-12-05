3a507ed04c02be220e16cd5eb7dfd88f7f9d3811
VIT <Vit.Por@bk.ru>
2022-12-05 22:46:33 +0300

    newRep

--- BackupsExtra.Lib/FileSystem/Abstract/IVirtualDirectoryFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backups.FileSystem.Abstract
-{
-    public interface IVirtualDirectoryFactory<TDir>
-        where TDir : class, IVirtualDirectory
-    {
-        TDir Create(TDir dir, string name);
-        TDir Create(string fullPath);
-    }
-}
--- BackupsExtra.Lib/FileSystem/Abstract/IVisitor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backups.FileSystem
-{
-    public interface IVisitor
-    {
-        public void Visit(IVirtualFile file);
-        public void Visit(IVirtualDirectory file);
-        void AddParent(string relativePath);
-    }
-}
